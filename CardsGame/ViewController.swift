741ebc63c130a56e629833ff1b99d359a874e3f8
Vladislav <sfx.nontage@gmail.com>
2020-03-22 23:20:01 +0300

    Initial Commit

---
+++ CardsGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardsGame
+//
+//  Created by Steven Kirke on 22.03.2020.
+//  Copyright © 2020 Steven Kirke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
